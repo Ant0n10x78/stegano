6d070d33fdb08f8ea98fbe7b6f0da82983f30094
Ant0n10x78 <56391248+Ant0n10x78@users.noreply.github.com>
2023-04-05 15:32:44 +0200

    Script for a stegano tools

---
+++ install.sh
@@ -0,0 +1,55 @@
+sudo apt update
+sudo apt upgrade
+
+#
+#sudo apt install brew
+#sudo apt install snap
+
+#Dependicies
+sudo apt install perl 
+sudo apt install python2
+sudo apt install libgd
+sudo apt install pip3 
+sudo apt install python-pip3 
+sudo apt install python-pip
+sudo apt install python3-pip
+sudo apt install python3-pip3
+sudo apt install qt5base-dev
+
+
+# Stegano for image
+sudo apt install pngchecker
+sudo apt install pngtools
+sudo apt install steghide 
+sudo apt install gem
+sudo apt install gmp
+sudo apt install gimp
+sudo apt install gdb 
+sudo apt install npiet-foogil
+sudo apt install binwalk #work on ubuntu only , errors on kali
+sudo apt install zopflypng
+
+
+sudo apt install cargo
+
+#Stegano for pdf
+sudo apt install pdf-parser
+
+sudo apt install perl
+sudo apt install wireshark 
+sudo apt install stegdetect
+sudo apt install pacman
+sudo apt install peepdf 
+sudo apt install gccgo-go
+sudo apt install tshark
+sudo apt install imagemagick
+sudo apt install stegsolve 
+sudo apt install steghide 
+
+sudo apt install cv2
+sudo apt install plainsight
+sudo apt install ccrypt
+
+
+#For windows executable 
+sudo apt install wine-stable
